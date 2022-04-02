9fa2f277a4e802f821445797ba260146bb6f2dd0
Max-Martin Sherie <maxmartinsherie@gmail.com>
2022-04-02 11:33:22 +0200

    asnee

---
+++ Flatten Penguin/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadSceneOnClick : MonoBehaviour
+{
+    [SerializeField] private int m_nextSceneIndex;
+    void Update() {if(Input.GetKeyDown(KeyCode.Mouse0))SceneManager.LoadScene(m_nextSceneIndex);}
+}
