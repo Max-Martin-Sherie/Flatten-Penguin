6f12522aeeb132aa3fcb1fa30878f84726c83f7d
nanutra <nicolas.mendiboure@gmail.com>
2022-04-03 05:24:32 +0200

    GoodEnding

--- Flatten Penguin/Assets/Scripts/GoodEnding.cs
+++ Flatten Penguin/Assets/Scripts/GoodEnding.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GoodEnding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    private Volume m_victoryVolume;
+    [SerializeField] private float m_speed;
+
+    private void Start() => m_victoryVolume = gameObject.GetComponent<Volume>();
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(m_victoryVolume.weight > 0)
+            m_victoryVolume.weight -= m_speed * Time.deltaTime;
+
+        if (m_victoryVolume.weight < 0)
+        {
+            Destroy(this);
+        }
     }
 }
